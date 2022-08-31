9e5326c91dff8724cd819f2adf58cdbbee58fa47
thiagosprestes <thiago.santos.prestes@gmail.com>
2022-08-31 18:11:39 -0300

    read card and get initial data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,9 @@
+global.Buffer = global.Buffer || require("buffer").Buffer;
+import TLV from "node-tlv";
 import React, { useContext, useEffect, useState } from "react";
 import { TouchableWithoutFeedback, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import nfcManager, { NfcAdapter, NfcTech } from "react-native-nfc-manager";
 import Animated, {
   Extrapolate,
   interpolate,
@@ -8,11 +11,14 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
+import { ApduCommand } from "../../ApduCommand";
+import { ApduResponse } from "../../ApduResponse";
 import { Card } from "../../components/Card";
 import { CardBack } from "../../components/Card/CardBack";
 import { CardFront } from "../../components/Card/CardFront";
 import { StepContainer } from "../../components/CardSteps/StepContainer";
 import { CardContext, CardContextProps } from "../../contexts/card";
+import { useReadCard } from "../../hooks/useReadCard";
 import { CardBrand, getCardBrand } from "../../utils/cardBrands";
 import styles from "./styles";
 
@@ -94,10 +100,40 @@ export const Home = () => {
     };
   }, []);
 
+  nfcManager.start();
+
+  const { getCardData } = useReadCard();
+
+  async function readNdef() {
+    try {
+      console.warn("Read tag");
+
+      // register for the NFC tag with NDEF in it
+      await nfcManager.requestTechnology(NfcTech.IsoDep, {
+        isReaderModeEnabled: true,
+        readerModeDelay: 10,
+        readerModeFlags:
+          NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
+      });
+
+      // the resolved tag object will contain `ndefMessage` property
+      await nfcManager.getTag();
+
+      await getCardData();
+
+      console.warn("Tag found");
+    } catch (ex) {
+      console.warn("Oops!", ex);
+    } finally {
+      // stop the nfc scanning
+      nfcManager.cancelTechnologyRequest();
+    }
+  }
+
   return (
     <View style={styles.container}>
       <GestureDetector gesture={gesture}>
-        <TouchableWithoutFeedback onPress={flipCard}>
+        <TouchableWithoutFeedback onPress={readNdef}>
           <Animated.View style={(styles.cardContainer, rotateCardStyle)}>
             <Animated.View style={flipFront}>
               <Card cardBrand={cardBrand}>
